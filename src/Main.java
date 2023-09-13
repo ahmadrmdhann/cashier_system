101eb3d8ec6416e0fba9702ff451a083f25298ac
Sythaz <syafiqaldiansyah@gmail.com>
2023-09-13 18:20:49 +0700

    Added uangKembalian

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,43 @@
-package src;
 
-/**
- * InnerMain
- */
+import java.util.Scanner;
+
 public class Main {
-
-static void myMenu() {
-        System.out.println("+---------------------------------------+");
-        System.out.println("|                  MENU                 |");
-        System.out.println("+---------------- Makanan --------------+");
-        System.out.println("| 1  | Nasi\t\t\t\t|");
-        System.out.println("| 2  | Sate\t\t\t\t|");
-        System.out.println("| 3  | Sayur Lodeh\t\t\t|");
-        System.out.println("| 4  | Sayur Asem\t\t\t|");
-        System.out.println("| 5  | Ayam Goreng\t\t\t|");
-        System.out.println("|---------------- Minuman --------------|");
-        System.out.println("| 6  | Teh Anget\t\t\t|");
-        System.out.println("| 7  | Es Teh\t\t\t\t|");
-        System.out.println("| 8  | Jeruk Anget\t\t\t|");
-        System.out.println("| 9  | Es Jeruk\t\t\t\t|");
-        System.out.println("| 10 | Susu Soda\t\t\t|");
-        System.out.println("+---------------------------------------+");
-}
     public static void main(String[] args) {
-    myMenu();
-}
-    
+        Scanner sc = new Scanner(System.in);
+        String namaPembeli;
+        int jumlahNasiGoreng, jumlahMieGoreng, jumlahSate, jumlahAyamGoreng, jumlahAyamGeprek; 
+        double totalHarga, uangPembeli, uangKembalian;
+        System.out.print("Silahkan masukkan nama pembeli : ");
+        namaPembeli = sc.nextLine();
+        System.out.print("Berapa banyak anda ingin memesan Nasi Goreng (Rp. 10.000/pcs) : ");
+        jumlahNasiGoreng = sc.nextInt();
+        System.out.print("Berapa banyak anda ingin memesan Mie Goreng (Rp. 12.000/pcs): ");
+        jumlahMieGoreng = sc.nextInt();
+        System.out.print("Berapa banyak anda ingin memesan Sate (Rp. 15.000/pcs) : ");
+        jumlahSate = sc.nextInt();
+        System.out.print("Berapa banyak anda ingin memesan Ayam Goreng (Rp. 12.0000/pcs) : ");
+        jumlahAyamGoreng = sc.nextInt();
+        System.out.print("Berapa banyak anda ingin memesan Ayam Geprek (Rp. 10.000/pcs) : ");
+        jumlahAyamGeprek = sc.nextInt();
+        
+        totalHarga = jumlahNasiGoreng * 10000 + jumlahMieGoreng * 12000 + jumlahSate * 15000 + jumlahAyamGoreng * 12000 + jumlahAyamGeprek * 10000;
+
+        System.out.println("Total harga yang harus dibayarkan : " + totalHarga);
+        System.out.print("Masukkan uang anda : ");
+        uangPembeli = sc.nextDouble();
+
+        uangKembalian = uangPembeli - totalHarga;
+
+        System.out.println("\n\n----------------------Struk-----------------------");
+        System.out.println("Jumlah nasi goreng yang anda pesan sebanyak : " + jumlahNasiGoreng);
+        System.out.println("Jumlah mie goreng yang anga pesan sebanyak : " + jumlahMieGoreng);
+        System.out.println("Jumlah sate yang anda pesan sebanyak : " + jumlahSate);
+        System.out.println("Jumlah ayam goreng yang anda pesan sebanyak : " + jumlahAyamGoreng);
+        System.out.println("Jumlah ayam geprek yang anda pesan sebanyak : " + jumlahAyamGeprek);
+        System.out.println("Total harga yang harus anda bayar sebesar : Rp. " + totalHarga);
+        System.out.println("Jumlah uang yang dibayarkan: Rp. " + uangPembeli);
+        System.out.println("Total uang kembalian : Rp. " + uangKembalian);
+        System.out.println(namaPembeli + " terima kasih sudah berbelanja di restoran kami");
+        System.out.println("--------------------------------------------------------------");
+    }
 }

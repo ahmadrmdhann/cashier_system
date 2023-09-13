d0369d5534cbaf6dc47866b3d5317208271b287b
Sythaz <syafiqaldiansyah@gmail.com>
2023-09-13 17:12:34 +0700

    Added myMenu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+package src;
+
+/**
+ * InnerMain
+ */
 public class Main {
+
+static void myMenu() {
+        System.out.println("+---------------------------------------+");
+        System.out.println("|                  MENU                 |");
+        System.out.println("+---------------- Makanan --------------+");
+        System.out.println("| 1  | Nasi\t\t\t\t|");
+        System.out.println("| 2  | Sate\t\t\t\t|");
+        System.out.println("| 3  | Sayur Lodeh\t\t\t|");
+        System.out.println("| 4  | Sayur Asem\t\t\t|");
+        System.out.println("| 5  | Ayam Goreng\t\t\t|");
+        System.out.println("|---------------- Minuman --------------|");
+        System.out.println("| 6  | Teh Anget\t\t\t|");
+        System.out.println("| 7  | Es Teh\t\t\t\t|");
+        System.out.println("| 8  | Jeruk Anget\t\t\t|");
+        System.out.println("| 9  | Es Jeruk\t\t\t\t|");
+        System.out.println("| 10 | Susu Soda\t\t\t|");
+        System.out.println("+---------------------------------------+");
+}
     public static void main(String[] args) {
+    myMenu();
+}
     
-    }
 }

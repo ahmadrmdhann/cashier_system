e8fb4c6b2625f8b13dd372799b8e3109b91ee293
ahmadrmdhann <ahmadramadhansqorpio04@gmail.com>
2023-10-02 19:44:08 +0700

    Add Fitur Reservasi

--- src/Main.java
+++ src/Main.java
@@ -10,8 +10,13 @@ public class Main {
         String namaPembeli;
         int jumlahNasiGoreng, jumlahMieGoreng, jumlahSate, jumlahAyamGoreng, jumlahAyamGeprek; 
         double totalHarga, uangPembeli, uangKembalian;
+        boolean meja01 = true, meja02 = true, meja03 = true;
+        boolean mejaStatusTrue = true;
+        boolean mejaStatusFalse = false;
+        String mejaMana;
+        int jumlahOrang;
 
-        //Sistem registrasi
+        // Sistem registrasi
 
         System.out.print("Masukkan username anda: ");
         username = sc.nextLine();
@@ -25,7 +30,8 @@ public class Main {
             System.exit(0);
         }
 
-        // fitur order menu dan cetak struk
+        // Fitur order menu dan cetak struk
+
         System.out.println("\nSelamat datang di restoran kami!");
         System.out.print("Silahkan masukkan nama anda untuk memulai memesan : ");
         namaPembeli = sc.nextLine();
@@ -59,5 +65,39 @@ public class Main {
         System.out.println("Total uang kembalian : Rp. " + uangKembalian);
         System.out.println(namaPembeli + " terima kasih sudah berbelanja di restoran kami");
         System.out.println("--------------------------------------------------------------");
+
+        // Fitur reservasi meja
+        if (meja01 == true) {
+            System.out.println("\nMeja 1 tersedia!");
+        }
+        if (meja02 == true) {
+            System.out.println("Meja 2 tersedia!");
+        }
+        if (meja03 == true) {
+            System.out.println("Meja 3 tersedia!");
+        }
+        System.out.println("Pilihlah meja mana yang ingin anda tempati (1/2/3) ?");
+        mejaMana = sc.nextLine();
+        System.out.print("Anda ingin melakukan reservasi untuk berapa orang : ");
+        jumlahOrang = sc.nextInt();
+
+
+        switch (mejaMana) {
+            case "1":
+                System.out.println("Anda telah melakukan reservasi untuk meja 1 untuk "+jumlahOrang+" orang");
+                meja01 = mejaStatusFalse;
+                // System.out.println(meja01);
+                break;
+            case "2":
+                System.out.println("Anda telah melakukan reservasi untuk meja 2 untuk "+jumlahOrang+" orang");
+                meja02 = mejaStatusFalse;
+                // System.out.println(meja02);
+                break;
+            case "3":
+                System.out.println("Anda telah melakukan reservasi untuk3 meja 3 untuk "+jumlahOrang+" orang");
+                meja03 = mejaStatusFalse;
+                // System.out.println(meja03);
+                break;
+        }
     }
 }

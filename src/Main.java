f1adb53f0918a11ca526a1acb451a0ce7934ba3b
Sythaz <syafiqaldiansyah@gmail.com>
2023-10-12 16:22:23 +0700

    Rewrite List Menu Feature

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
+        //Inisialisasi Sistem Registrasi
         String username, password;
         String usernameOwner = "Owner";
         String passwordOwner = "owner123";
-        String namaPembeli;
-        int jumlahNasiGoreng, jumlahMieGoreng, jumlahSate, jumlahAyamGoreng, jumlahAyamGeprek; 
-        double totalHarga, uangPembeli, uangKembalian;
+
+        //Inisialisasi Fitur Daftar Menu
+        int jumlahMenu, banyakSate = 0, banyakSayurLodeh = 0, banyakSayurAsem = 0, banyakAyamGeprek = 0, banyakTeh = 0, banyakJeruk = 0, banyakSusuSoda = 0, totalNasgor = 0, totalSate = 0, totalSayurLodeh = 0, totalSayurAsem = 0, totalAyamGeprek = 0, totalTeh = 0, totalJeruk = 0, totalSusuSoda = 0, banyakNasgor = 0, totalHarga = 0, uangKembalian = 0;
+        int uangPembeli;
+
+        //Inisialisasi Fitur Reservasi Meja
         boolean meja01 = true, meja02 = true, meja03 = true;
         boolean mejaStatusTrue = true; //Persiapan meja kembali tersedia
         boolean mejaStatusFalse = false;
@@ -32,39 +37,149 @@ public class Main {
 
         // Fitur order menu dan cetak struk
 
-        System.out.println("\nSelamat datang di restoran kami!");
-        System.out.print("Silahkan masukkan nama anda untuk memulai memesan : ");
-        namaPembeli = sc.nextLine();
-        System.out.print("Berapa banyak anda ingin memesan Nasi Goreng (Rp. 10.000/pcs) : ");
-        jumlahNasiGoreng = sc.nextInt();
-        System.out.print("Berapa banyak anda ingin memesan Mie Goreng (Rp. 12.000/pcs): ");
-        jumlahMieGoreng = sc.nextInt();
-        System.out.print("Berapa banyak anda ingin memesan Sate (Rp. 15.000/pcs) : ");
-        jumlahSate = sc.nextInt();
-        System.out.print("Berapa banyak anda ingin memesan Ayam Goreng (Rp. 12.0000/pcs) : ");
-        jumlahAyamGoreng = sc.nextInt();
-        System.out.print("Berapa banyak anda ingin memesan Ayam Geprek (Rp. 10.000/pcs) : ");
-        jumlahAyamGeprek = sc.nextInt();
+        System.out.println("\n\nSelamat datang di restoran kami!\n");
+
+        System.out.println("\t\t+-----------------------------------------+");
+        System.out.println("\t\t|                   MENU                  |");
+        System.out.println("\t\t+-Kode------------ Makanan ---------------+");
+        System.out.println("\t\t|  A1  | Nasi Goreng\t\tRp. 10.000|");
+        System.out.println("\t\t|  A2  | Sate\t\t\tRp. 15.000|");
+        System.out.println("\t\t|  A3  | Sayur Lodeh\t\tRp. 8.000 |");
+        System.out.println("\t\t|  A4  | Sayur Asem\t\tRp. 8.000 |");
+        System.out.println("\t\t|  A5  | Ayam Geprek\t\tRp. 10.000|");
+        System.out.println("\t\t|----------------- Minuman ---------------|");
+        System.out.println("\t\t|  B1  | Teh\t\t\tRp. 5.000 |");
+        System.out.println("\t\t|  B2  | Jeruk\t\t\tRp. 5.000 |");
+        System.out.println("\t\t|  B3  | Susu Soda\t\tRp. 5.000 |");
+        System.out.println("\t\t+-----------------------------------------+\n");
+
+        System.out.print("Berapa jenis menu makanan dan minuman yang ingin anda pesan? ");
+        jumlahMenu = sc.nextInt();
         
-        totalHarga = jumlahNasiGoreng * 10000 + jumlahMieGoreng * 12000 + jumlahSate * 15000 + jumlahAyamGoreng * 12000 + jumlahAyamGeprek * 10000;
+        System.out.println("\nUntuk memasukkan pilihan makanan yang ingin dipesan, gunakanlah KODE!");
+
+        int i = 1;
+        while (i <= jumlahMenu) {
+            System.out.printf("Apa yang ingin anda pesan pada Menu #%d? ", i);
+            String scMenu = sc.next();
+            
+            if (scMenu.equalsIgnoreCase("A1")) {
+                System.out.print("Berapa banyak anda ingin memesan Nasi Goreng? ");
+                banyakNasgor = sc.nextInt();
+                while (banyakNasgor <= 0) {
+                    System.out.print("\nNominal menu tidak bisa kurang dari 1!\nBerapa banyak anda ingin memesannya? ");
+                    banyakNasgor = sc.nextInt();
+                }
+                totalNasgor = banyakNasgor * 10000;
+                i++;
+                continue;
+            } else if (scMenu.equalsIgnoreCase("A2")) {
+                System.out.print("Berapa banyak anda ingin memesan Sate? ");
+                banyakSate = sc.nextInt();
+                while (banyakSate <= 0) {
+                    System.out.print("\nNominal menu tidak bisa kurang dari 1!\nBerapa banyak anda ingin memesannya? ");
+                    banyakSate = sc.nextInt();
+                }
+                totalSate = banyakSate * 15000;
+                i++;
+                continue;
+            } else if (scMenu.equalsIgnoreCase("A3")) {
+                System.out.print("Berapa banyak anda ingin memesan Sayur Lodeh? ");
+                banyakSayurLodeh = sc.nextInt();
+                while (banyakSayurLodeh <= 0) {
+                    System.out.print("\nNominal menu tidak bisa kurang dari 1!\nBerapa banyak anda ingin memesannya? ");
+                    banyakSayurLodeh = sc.nextInt();
+                }
+                totalSayurLodeh = banyakSayurLodeh * 8000;
+                i++;
+                continue;
+            } else if (scMenu.equalsIgnoreCase("A4")) {
+                System.out.print("Berapa banyak anda ingin memesan Sayur Asem? ");
+                banyakSayurAsem = sc.nextInt();
+                while (banyakSayurAsem <= 0) {
+                    System.out.print("\nNominal menu tidak bisa kurang dari 1!\nBerapa banyak anda ingin memesannya? ");
+                    banyakSayurAsem = sc.nextInt();
+                }
+                totalSayurAsem = banyakSayurAsem * 8000;
+                i++;
+                continue;
+            } else if (scMenu.equalsIgnoreCase("A5")) {
+                System.out.print("Berapa banyak anda ingin memesan Ayam Geprek? ");
+                banyakAyamGeprek = sc.nextInt();
+                while (banyakAyamGeprek <= 0) {
+                    System.out.print("\nNominal menu tidak bisa kurang dari 1!\nBerapa banyak anda ingin memesannya? ");
+                    banyakAyamGeprek = sc.nextInt();
+                }
+                totalAyamGeprek = banyakAyamGeprek * 10000;
+                i++;
+                continue;
+            } else if (scMenu.equalsIgnoreCase("B1")) {
+                System.out.print("Berapa banyak anda ingin memesan Teh? ");
+                banyakTeh = sc.nextInt();
+                while (banyakTeh <= 0) {
+                    System.out.print("\nNominal menu tidak bisa kurang dari 1!\nBerapa banyak anda ingin memesannya? ");
+                    banyakTeh = sc.nextInt();
+                }
+                System.out.print("Teh dingin/anget? ");
+                String opsiTeh = sc.next();
+                totalTeh = banyakTeh * 5000;
+                i++;
+                continue;
+            } else if (scMenu.equalsIgnoreCase("B2")) {
+                System.out.print("Berapa banyak anda ingin memesan minuman Jeruk? ");
+                sc.nextInt(); //Formalitas penanyaan dingin/panas sehingga tidak perlu di inisialisasi
+                while (banyakJeruk <= 0) {
+                    System.out.print("\nNominal menu tidak bisa kurang dari 1!\nBerapa banyak anda ingin memesannya? ");
+                    banyakJeruk = sc.nextInt();
+                }
+                System.out.print("Jeruk dingin/anget? ");
+                sc.next(); //Formalitas penanyaan dingin/panas sehingga tidak perlu di inisialisasi
+                totalJeruk = banyakJeruk * 12000;
+                i++;
+                continue;
+            } else if (scMenu.equalsIgnoreCase("B3")) {
+                System.out.print("Berapa banyak anda ingin memesan Susu Soda? ");
+                banyakSusuSoda = sc.nextInt();
+                while (banyakSusuSoda <= 0) {
+                    System.out.print("\nNominal menu tidak bisa kurang dari 1!\nBerapa banyak anda ingin memesannya? ");
+                    banyakSusuSoda = sc.nextInt();
+                }
+                totalSusuSoda = banyakSusuSoda * 12000;
+                i++;
+                continue;
+            } else {
+                System.out.print("\nMenu tidak valid, silahkan isi menu kembali..\n");
+            }  
+        }
+        totalHarga = totalNasgor + totalSate + totalSayurLodeh + totalSayurAsem + totalAyamGeprek + totalTeh + totalJeruk + totalSusuSoda;
 
         System.out.println("Total harga yang harus dibayarkan : " + totalHarga);
         System.out.print("Masukkan uang anda : ");
-        uangPembeli = sc.nextDouble();
+        uangPembeli = sc.nextInt();
+
+        while (uangPembeli < totalHarga) {
+            System.out.print("\nMaaf uang anda kurang.\nSilahkan memasukkan nominal lain: ");
+            uangPembeli = sc.nextInt();
+
+        }
 
         uangKembalian = uangPembeli - totalHarga;
-
-        System.out.println("\n\n----------------------Struk-----------------------");
-        System.out.println("Jumlah nasi goreng yang anda pesan sebanyak : " + jumlahNasiGoreng);
-        System.out.println("Jumlah mie goreng yang anga pesan sebanyak : " + jumlahMieGoreng);
-        System.out.println("Jumlah sate yang anda pesan sebanyak : " + jumlahSate);
-        System.out.println("Jumlah ayam goreng yang anda pesan sebanyak : " + jumlahAyamGoreng);
-        System.out.println("Jumlah ayam geprek yang anda pesan sebanyak : " + jumlahAyamGeprek);
-        System.out.println("Total harga yang harus anda bayar sebesar : Rp. " + totalHarga);
-        System.out.println("Jumlah uang yang dibayarkan: Rp. " + uangPembeli);
-        System.out.println("Total uang kembalian : Rp. " + uangKembalian);
-        System.out.println(namaPembeli + " terima kasih sudah berbelanja di restoran kami");
-        System.out.println("--------------------------------------------------------------");
+        
+        System.out.println("\n\n------------------------Struk------------------------");
+        System.out.printf("Nasi Goreng\t: %d  x  Rp. 10.000\t= Rp. %d\n", banyakNasgor, totalNasgor);
+        System.out.printf("Sate\t\t: %d  x  Rp. 15.000\t= Rp. %d\n", banyakSate, totalSate);
+        System.out.printf("Sayur Lodeh\t: %d  x  Rp. 8.000\t= Rp. %d\n", banyakSayurLodeh, totalSayurLodeh);
+        System.out.printf("Sayur Asem\t: %d  x  Rp. 8.000\t= Rp. %d\n", banyakSayurAsem, totalSayurAsem);
+        System.out.printf("Ayam Geprek\t: %d  x  Rp. 10.000\t= Rp. %d\n", banyakAyamGeprek, totalAyamGeprek);
+        System.out.printf("Teh\t\t: %d  x  Rp. 5.000\t= Rp. %d\n", banyakAyamGeprek, totalAyamGeprek);
+        System.out.printf("Jeruk\t\t: %d  x  Rp. 5.000\t= Rp. %d\n", banyakAyamGeprek, totalAyamGeprek);
+        System.out.printf("Susu Soda\t: %d  x  Rp. 5.000\t= Rp. %d\n", banyakAyamGeprek, totalAyamGeprek);
+        System.out.println("-----------------------------------------------------");
+        System.out.println("Total Akhir\t:\t\t\t  Rp. " + totalHarga);
+        System.out.println("Cash\t\t:\t\t\t  Rp. " + uangPembeli);
+        System.out.println("Kembalian\t:\t\t\t  Rp. " + uangKembalian);
+        System.out.println("-----------------------------------------------------");
+        System.out.println("\tTerima Kasih Atas Kunjungan Anda");
 
         // Fitur reservasi meja
         if (meja01 == true) {
